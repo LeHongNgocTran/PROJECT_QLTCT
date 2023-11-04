1e79247b24ae72af149e016a20fc78df0386f79f
Lê Hồng Ngọc Trân <93925604+LeHongNgocTran@users.noreply.github.com>
2023-11-04 13:52:14 +0700

    add data

--- BackEnd/app/config/index.js
+++ BackEnd/app/config/index.js
@@ -3,7 +3,8 @@ const config = {
         port : process.env.PORT || 3001
     },
     db : {
-        uri : process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/quanlytaucaotoc'    
+        uri : process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/chỗ này là tên collection nè'     // chỗ này là đường link để connect với mongo
+// đổi url là sẽ connect đc với mongdo bên m
     }
 }
 
